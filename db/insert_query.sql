dea47bf9c2c6ef4e28abba18e1bffdda0627911d
STian <s.tyan.08.1988@gmail.ru>
2022-07-10 18:52:09 +0300

    update

--- db/insert_query.sql
+++ db/insert_query.sql
@@ -29,333 +29,103 @@ although the former describes all music that is popular and includes many dispar
 );
 
 insert into artist (name, birthday, country)
-values ('Bruce the Shark', '01.01.2010', 'Russia'
-);
-
-insert into artist (name, birthday, country)
-values ('Bob Parr (Mr. Incredible)', '02.02.2012', 'USA'
-);
-
-insert into artist (name, birthday, country)
-values ('Nemo', '01.01.2010', 'Netherlands'
-);
-
-insert into artist (name, birthday, country)
-values ('Remy', '14.10.1956', 'Italy'
-);
-
-insert into artist (name, birthday, country)
-values ('Sulley Sullivan', '24.09.1972', 'Moldova'
-);
-
-insert into artist (name, birthday, country)
-values ('Mike Wazowski', '18.08.2001', 'Vatican'
-);
-
-insert into artist (name, birthday, country)
-values ('Merida', '05.07.2005', 'Scotland'
-);
-
-insert into artist (name, birthday, country)
-values ('Joy', '05.05.2015', 'Inside Out'
-);
+values ('Bruce the Shark', '01.01.2010', 'Russia'),
+('Bob Parr (Mr. Incredible)', '02.02.2012', 'USA'),
+('Nemo', '01.01.2010', 'Netherlands'),
+('Remy', '14.10.1956', 'Italy'),
+('Sulley Sullivan', '24.09.1972', 'Moldova'),
+('Mike Wazowski', '18.08.2001', 'Vatican'),
+('Merida', '05.07.2005', 'Scotland'),
+('Joy', '05.05.2015', 'Inside Out')
 
 insert into mix_tape (name, release)
-values ('Best metall tracks', '01.06.2021'
-);
-
-insert into mix_tape (name, release)
-values ('Best tracks ever', '06.08.2030'
-);
-
-insert into mix_tape (name, release)
-values ('My favorite tracks', '04.12.2022'
-);
-
-insert into mix_tape (name, release)
-values ('Это репчик, чувак', '14.10.1952'
-);
-
-insert into mix_tape (name, release)
-values ('For train', '21.11.2010'
-);
-
-insert into mix_tape (name, release)
-values ('For sleep', '01.06.2001'
-);
-
-insert into mix_tape (name, release)
-values ('For my wedding', '20.07.1927'
-);
-
-insert into mix_tape (name, release)
-values ('Hits', '01.12.2025'
-);
+values 
+('Best metall tracks', '01.06.2021'),
+('Best tracks ever', '06.08.2030'),
+('My favorite tracks', '04.12.2022'),
+('Это репчик, чувак', '14.10.1952'),
+('For train', '21.11.2010'),
+('For sleep', '01.06.2001'),
+('For my wedding', '20.07.1927'),
+('Hits', '01.12.2025'),
 
 insert into album (name, release)
-values ('First love', '01.01.2001'
-);
-
-insert into album (name, release)
-values ('Second love', '02.02.2002'
-);
-
-insert into album (name, release)
-values ('Last love', '03.03.2003'
-);
-
-insert into album (name, release)
-values ('My love', '04.04.2004'
-);
-
-insert into album (name, release)
-values ('With love', '05.05.2005'
-);
-
-insert into album (name, release)
-values ('For love', '06.06.2006'
-);
-
-insert into album (name, release)
-values ('Love in mi hart', '07.07.2007'
-);
-
-insert into album (name, release)
-values ('About love', '08.08.2008'
-);
+values 
+('First love', '01.01.2001'),
+('Second love', '02.02.2002'),
+('Last love', '03.03.2003'),
+('My love', '04.04.2004'),
+('With love', '05.05.2005'),
+('For love', '06.06.2006'),
+('Love in mi hart', '07.07.2007'),
+('About love', '08.08.2008');
 
 insert into track (name, album, mix_tape, length)
-values ('one', '1', '8', '180'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('two', '2', '7', '260'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('three', '3', '6', '186'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('four', '4', '5', '187'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('five', '5', '4', '183'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('six', '6', '3', '183'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('seven', '7', '2', '189'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('eight', '8', '1', '240'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('nine', '1', '8', '245'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('ten', '2', '7', '163'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('eleven', '3', '6', '134'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('twelve', '4', '5', '246'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('thirteen', '5', '4', '247'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('fourteen', '6', '3', '249'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('fifteen', '7', '2', '123'
-);
-
-insert into track (name, album, mix_tape, length)
-values ('sixteen', '8', '1', '154'
-);
+values ('one', '1', '8', '180'),
+('two', '2', '7', '260'),
+('three', '3', '6', '186'),
+('four', '4', '5', '187'),
+('five', '5', '4', '183'),
+('six', '6', '3', '183'),
+('seven', '7', '2', '189'),
+('eight', '8', '1', '240'),
+('nine', '1', '8', '245'),
+('ten', '2', '7', '163'),
+('eleven', '3', '6', '134'),
+('twelve', '4', '5', '246'),
+('thirteen', '5', '4', '247'),
+('fourteen', '6', '3', '249'),
+('fifteen', '7', '2', '123'),
+('sixteen', '8', '1', '154');
 
 insert into artist_ganre (artist_id, ganre_id)
-values ('1', '1'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('2', '1'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('3', '1'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('4', '2'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('5', '2'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('6', '3'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('7', '4'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('8', '3'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('1', '4'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('1', '2'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('8', '1'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('8', '2'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('4', '1'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('3', '5'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('6', '5'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('8', '5'
-);
-
-insert into artist_ganre (artist_id, ganre_id)
-values ('5', '5'
-);
+values 
+('1', '1'),
+('2', '1'),
+('3', '1'),
+('4', '2'),
+('5', '2'),
+('6', '3'),
+('7', '4'),
+('8', '3'),
+('1', '4'),
+('1', '2'),
+('8', '1'),
+('8', '2'),
+('4', '1'),
+('3', '5'),
+('6', '5'),
+('8', '5'),
+('5', '5');
 
 insert into artist_ulbum (artist_id, album_id)
-values ('1', '1'
-);
-
-insert into artist_ulbum (artist_id, album_id)
-values ('2', '2'
-);
-
-insert into artist_ulbum (artist_id, album_id)
-values ('3', '3'
-);
-
-insert into artist_ulbum (artist_id, album_id)
-values ('4', '4'
-);
-
-insert into artist_ulbum (artist_id, album_id)
-values ('5', '5'
-);
-
-insert into artist_ulbum (artist_id, album_id)
-values ('6', '6'
-);
-
-insert into artist_ulbum (artist_id, album_id)
-values ('7', '7'
-);
-
-insert into artist_ulbum (artist_id, album_id)
-values ('1', '2'
-);
-
-insert into artist_ulbum (artist_id, album_id)
-values ('1', '3'
-);
-
-insert into artist_ulbum (artist_id, album_id)
-values ('1', '4'
-);
-
-insert into artist_ulbum (artist_id, album_id)
-values ('8', '1'
-);
-
-insert into artist_ulbum (artist_id, album_id)
-values ('8', '7'
-);
+values 
+('1', '1'),
+('2', '2'),
+('3', '3'),
+('4', '4'),
+('5', '5'),
+('6', '6'),
+('7', '7'),
+('1', '2'),
+('1', '3'),
+('1', '4'),
+('8', '1'),
+('8', '7');
 
 insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('1', '1'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('2', '2'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('3', '3'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('4', '4'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('5', '5'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('6', '6'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('7', '7'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('8', '8'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('1', '8'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('2', '7'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('3', '6'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('4', '5'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('5', '4'
-);
-
-insert into artist_mix_tape (artist_id, mix_tape_id)
-values ('6', '3'
-);
+values 
+('1', '1'),
+('2', '2'),
+('3', '3'),
+('4', '4'),
+('5', '5'),
+('6', '6'),
+('7', '7'),
+('8', '8'),
+('1', '8'),
+('2', '7'),
+('3', '6'),
+('4', '5'),
+('5', '4'),
+('6', '3');
